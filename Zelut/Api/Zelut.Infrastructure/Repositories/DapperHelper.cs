abd8c1d868f0adbed58542379a797d5b28e79c1e
Hossein dinarvand <h.dinarvand@outlook.com>
2025-10-06 16:33:42 +0330

    feature : implement dapper helper

---
+++ Zelut/Api/Zelut.Infrastructure/Repositories/DapperHelper.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Zelut.Common.Helpers;
+
+namespace Zelut.Infrastructure.Repository;
+
+public class DapperHelper : IDapperHelper
+{
+    public async Task<IEnumerable<T>> RunAsync<T>(string connectionString, string sql_query, object parameters)
+    {
+        using (IDbConnection connection = new SqlConnection(connectionString))
+        {
+            var result = await connection.QueryAsync<T>(sql_query, parameters);
+            return result;
+        }
+    }
+
+    public async Task<IEnumerable<T>> RunAsync<T>(string connectionString, string sql_query)
+    {
+        using(IDbConnection connection = new SqlConnection(connectionString))
+        {
+            var result = await connection.QueryAsync<T>(sql_query);
+            return result;
+        }
+    }
+
+    public async Task<IEnumerable<T>> RunAsync<T>(string connectionString,string sql, SqlBuilder sqlBuilder)
+    {
+        using (IDbConnection connection = new SqlConnection(connectionString))
+        {
+            var template = sqlBuilder.AddTemplate(sql);
+            var result = await connection.QueryAsync<T>(template.RawSql, template.Parameters);
+            return result;
+        }
+    }
+}
---
+++ Zelut/Api/Zelut.Infrastructure/Repositories/EfRepository.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Zelute.Application.Repository;
+
+public class EfRepository<TEntity> : IRepository<TEntity> where TEntity : class
+{
+    #region Fields
+    private readonly ZelutDbContext _dbContext;
+    private readonly DbSet<TEntity> _dbSet;
+    #endregion
+
+    #region ctor
+    public EfRepository(ZelutDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = _dbContext.Set<TEntity>();
+    }
+    #endregion
+
+    #region  Methods
+    public async Task<TEntity> Add(TEntity entity)
+    {
+        var entity_entry = await _dbSet.AddAsync(entity);
+        return (TEntity) entity_entry.Entity;
+    }
+
+    public async Task AddRange(List<TEntity> entities)
+    {
+        await _dbSet.AddRangeAsync(entities);
+    }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+        var result = await _dbContext.SaveChangesAsync();
+        return result > 0;
+    }
+
+    #endregion
+}
