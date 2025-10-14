42011abed9b5d0ba473ed1be8a92b93e12487a11
Hossein dinarvand <h.dinarvand@outlook.com>
2025-10-14 10:37:03 +0330

    add Id for zelut product detail

--- Zelut/LandingPage/Zelut.LandingPage/DTOs/ZelutProductDetail.cs
+++ Zelut/LandingPage/Zelut.LandingPage/DTOs/ZelutProductDetail.cs
@@ -2,6 +2,7 @@
 
 public class ZelutProductDetail
 {
+    public int Id { get; set; }
     public string NameCollection { get; set; }
     public string NameModel { get; set; }
     public ZelutProductDetailDto ZelutDetail { get; set; }
