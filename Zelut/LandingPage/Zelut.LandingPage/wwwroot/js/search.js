df505487eb563da589110a29668bfafb991caa65
Mahshad Oliaie <mahshadoliaie@gmail.com>
2025-10-19 18:14:34 +0330

    debug search

--- Zelut/LandingPage/Zelut.LandingPage/wwwroot/js/search.js
+++ Zelut/LandingPage/Zelut.LandingPage/wwwroot/js/search.js
@@ -1,15 +1,14 @@
 
+const searchInput = document.getElementById("search")
 
 const searchBoxToggle = () => {
+  searchInput.value = ""
   document.querySelector(".searchBox").classList.toggle("hidden")
 }
 
 const searchBar = document.querySelector(".searchBtn")
 searchBar.addEventListener("click", searchBoxToggle)
 
-
-
-const searchInput = document.getElementById("search")
 
 searchInput.addEventListener("keyup", () => {
   if (event.target.value.trim() !== "") {
@@ -28,10 +27,16 @@ const renderSearchResult = (res) => {
   let temp;
   if (res && res.data.length > 0) {
     temp = res.data.map(item => {
-      return `<a href="/Product/${item.id}/${(item.zelutDetail.length>0) ? item.zelutDetail[0].id : ""}" class="p-3 w-full bg-gray-50 hover:bg-pastelGreen rounded-md mt-2 cursor-pointer">${item.nameModel}${(item.zelutDetail.length>0) ? "-" + item.zelutDetail[0].nameJadid : ""}</a>`
+      let newTemp;
+      if (item.zelutDetail && item.zelutDetail.length > 0) {
+        newTemp = item.zelutDetail.map(detail => {
+          return `<a href="/Product/${item.id}/${detail.id}" class="p-3 w-full bg-gray-50 hover:bg-pastelGreen rounded-md mt-2 cursor-pointer">${item.nameModel}-${detail.nameJadid}</a>`
+        }).join("")
+      }
+      return newTemp
     }).join("")
     document.getElementById("searchResult").innerHTML = temp
-  }else{
+  } else {
     document.getElementById("searchResult").innerHTML = '<p class="p-3 bg-gray-50 hover:bg-pastelGreen rounded-md mt-2 cursor-pointer">محصولی یافت نشد</p>'
 
   }
