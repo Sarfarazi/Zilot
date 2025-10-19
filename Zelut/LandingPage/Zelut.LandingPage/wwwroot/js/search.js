4ba58751a9b6a065c7348ad7015f253d424dffc9
Mahshad Oliaie <mahshadoliaie@gmail.com>
2025-10-19 17:56:02 +0330

    fix search

--- Zelut/LandingPage/Zelut.LandingPage/wwwroot/js/search.js
+++ Zelut/LandingPage/Zelut.LandingPage/wwwroot/js/search.js
@@ -8,21 +8,31 @@ const searchBar = document.querySelector(".searchBtn")
 searchBar.addEventListener("click", searchBoxToggle)
 
 
-// let Products = []
 
-// async function searchHandler() {
-// try {
-//   const res = await fetch("http://localhost:5067/api/zelut/product/get-products/0");
-//   if (!res.ok) {
-//     throw new Error(`HTTP error! status: ${res.status}`);
-//   }
-//   const data = await res.json();
-//   Products = data.data
-// } catch (error) {
-//   console.error("Fetch error:", error);
-// }
+const searchInput = document.getElementById("search")
 
-// }
+searchInput.addEventListener("keyup", () => {
+  if (event.target.value.trim() !== "") {
+    searchHandler(event.target.value)
+  }
+})
 
-// searchHandler()
 
+const searchHandler = (value) => {
+  fetch(`http://localhost:5067/search-products/${value.trim()}`)
+    .then(res => res.json())
+    .then(data => renderSearchResult(data))
+}
+
+const renderSearchResult = (res) => {
+  let temp;
+  if (res && res.data.length > 0) {
+    temp = res.data.map(item => {
+      return `<a href="/Product/${item.id}/${(item.zelutDetail.length>0) ? item.zelutDetail[0].id : ""}" class="p-3 w-full bg-gray-50 hover:bg-pastelGreen rounded-md mt-2 cursor-pointer">${item.nameModel}${(item.zelutDetail.length>0) ? "-" + item.zelutDetail[0].nameJadid : ""}</a>`
+    }).join("")
+    document.getElementById("searchResult").innerHTML = temp
+  }else{
+    document.getElementById("searchResult").innerHTML = '<p class="p-3 bg-gray-50 hover:bg-pastelGreen rounded-md mt-2 cursor-pointer">محصولی یافت نشد</p>'
+
+  }
+}
